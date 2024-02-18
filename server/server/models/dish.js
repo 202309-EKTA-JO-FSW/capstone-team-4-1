3acffc0c8530082e8f875ac9feeb905126ca9b9b
MahmoudRumaneh <mahmoudrmn@gmail.com>
2024-02-18 19:30:15 +0300

    change item model

---
+++ server/server/models/dish.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const dishSchema = new mongoose.Schema({
+  restaurant_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Dish", dishSchema);

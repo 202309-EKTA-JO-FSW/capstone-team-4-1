4050eb31beadeb47d324b6bdca5ea79252aa431a
danaomar <danaomaark@gmail.com>
2024-02-23 19:48:16 +0300

    build separate item and dish model

---
+++ server/server/models/item.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    dish: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dish',
+        required: true
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+      required: true
+    },
+    price: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    note: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Item", itemSchema);

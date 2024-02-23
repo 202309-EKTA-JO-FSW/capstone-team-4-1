601a5c56c944fbb1ed19991488833610c3ab7793
Omar Masoud <omarjaffar3@gmail.com>
2024-02-23 21:28:16 +0300

    Create Rider Routes

---
+++ server/server/routes/rider.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const riderController = require('../controllers/rider');
+
+router.get('/:riderId/orders', riderController.getOrders);
+router.get('/:riderId/orders/:orderId', riderController.getOrderById);
+
+module.exports = router;

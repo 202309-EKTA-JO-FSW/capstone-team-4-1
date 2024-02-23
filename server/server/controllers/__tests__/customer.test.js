76f944d4173a6b71071d2132a3fd58e814282f44
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-02-23 18:50:22 +0300

    Feat: add customer controller and test

---
+++ server/server/controllers/__tests__/customer.test.js
@@ -0,0 +1,330 @@
+/* eslint-disable no-undef */
+const customerController = require('../customer')
+const Restaurant = require('../../models/restaurant')
+const Order = require('../../models/order')
+const { Dish } = require('../../models/item_dish')
+const { Item } = require('../../models/item_dish')
+
+jest.mock('../../models/restaurant')
+jest.mock('../../models/rider')
+jest.mock('../../models/order')
+jest.mock('../../models/customer')
+jest.mock('../../models/item_dish')
+
+describe('CustomerController', () => {
+  let req, res
+
+  beforeEach(() => {
+    req = { params: {}, body: {} }
+    res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis()
+    }
+  })
+
+  describe('getAllRestaurants', () => {
+    it('should return all restaurants', async () => {
+      Restaurant.find.mockResolvedValue(['restaurant1', 'restaurant2'])
+      await customerController.getAllRestaurants(req, res)
+      expect(res.json).toHaveBeenCalledWith(['restaurant1', 'restaurant2'])
+    })
+  })
+
+  describe('getRestaurantById', () => {
+    it('should return a restaurant by id', async () => {
+      req.params.id = 'testId'
+      Restaurant.findById.mockResolvedValue('restaurant1')
+      await customerController.getRestaurantById(req, res)
+      expect(res.json).toHaveBeenCalledWith('restaurant1')
+    })
+  })
+
+  describe('getAllDishes', () => {
+    it('should return all Dishes', async () => {
+      Dish.find.mockResolvedValue(['Dish1', 'Dish2'])
+      await customerController.getAllDishes(req, res)
+      expect(res.json).toHaveBeenCalledWith({ dishes: ['Dish1', 'Dish2'] })
+    })
+  })
+
+  describe('getDishById', () => {
+    it('should return a dish by id', async () => {
+      req.params.dishId = 'testId'
+      Dish.findById.mockResolvedValue({
+        _id: 'testId',
+        restaurant: 'testRestaurantId'
+      })
+      Restaurant.findById.mockResolvedValue({
+        name: 'Test Restaurant'
+      })
+      await customerController.getDishById(req, res)
+      expect(res.json).toHaveBeenCalledWith({
+        dish: {
+          _id: 'testId',
+          restaurant: 'testRestaurantId'
+        },
+        restaurantName: 'Test Restaurant'
+      })
+    })
+  })
+
+  describe('getAllDishesOfRestaurant', () => {
+    beforeEach(() => {
+      req.params.restaurantId = 'testRestaurantId'
+    })
+    it('should return all dishes from the restaurant', async () => {
+      const mockDishes = [
+        {
+          _id: 'dishId1',
+          restaurant: 'testRestaurantId',
+          title: 'Dish 1',
+          description: 'Description 1',
+          image: 'Image 1',
+          price: 10,
+          category: 'Category 1'
+        },
+        {
+          _id: 'dishId2',
+          restaurant: 'testRestaurantId',
+          title: 'Dish 2',
+          description: 'Description 2',
+          image: 'Image 2',
+          price: 15,
+          category: 'Category 2'
+        }
+      ]
+      Dish.find.mockResolvedValue(mockDishes)
+      await customerController.getAllDishesOfRestaurant(req, res)
+      expect(Dish.find).toHaveBeenCalledWith({ restaurant: 'testRestaurantId' })
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith({ dishes: mockDishes })
+    })
+  })
+
+  describe('getAllOrdersByUserId', () => {
+    it('should return all orders for a user', async () => {
+      req.params.userId = 'testUserId'
+      Order.find.mockResolvedValue(['order1', 'order2'])
+      Order.find = jest.fn().mockResolvedValue(['order1', 'order2'])
+      await customerController.getAllOrdersByUserId(req, res)
+      expect(res.json).toHaveBeenCalledWith({ orders: ['order1', 'order2'] })
+    })
+  })
+
+  describe('addItemToCart', () => {
+    it('should add an item to the cart and return the updated order', async () => {
+      req.params.customerId = 'testCustomerId'
+      req.body = {
+        restaurantId: 'testRestaurantId',
+        dishId: 'testDishId',
+        quantity: 2,
+        note: 'Extra sauce'
+      }
+      Item.findOne.mockResolvedValue({
+        dish: { _id: 'testDishId', price: 10 }
+      })
+      Order.findOneAndUpdate.mockResolvedValue({
+        _id: 'testOrderId',
+        price: 20,
+        save: jest.fn()
+      })
+      await customerController.addItemToCart(req, res)
+      expect(Item.findOne).toHaveBeenCalledWith({ 'dish._id': 'testDishId' })
+      expect(Order.findOneAndUpdate).toHaveBeenCalledWith(
+        {
+          customer: 'testCustomerId',
+          restaurant: 'testRestaurantId',
+          status: 'Pending'
+        },
+        {
+          $push: {
+            items: {
+              'dish._id': 'testDishId',
+              quantity: 2,
+              price: 20,
+              note: 'Extra sauce'
+            }
+          }
+        },
+        { new: true, upsert: true }
+      )
+      expect(res.status).toHaveBeenCalledWith(201)
+      expect(res.json).toHaveBeenCalledWith(expect.objectContaining({
+        message: 'Item added to cart successfully',
+        order: expect.objectContaining({
+          _id: 'testOrderId',
+          price: 40
+        })
+      }))
+    })
+
+    it('should handle dish not found and return status 404', async () => {
+      req.params.customerId = 'testCustomerId'
+      req.body = {
+        restaurantId: 'testRestaurantId',
+        dishId: 'testDishId',
+        quantity: 2,
+        note: 'Extra sauce'
+      }
+      Item.findOne.mockResolvedValue(null)
+      await customerController.addItemToCart(req, res)
+      expect(Item.findOne).toHaveBeenCalledWith({ 'dish._id': 'testDishId' })
+      expect(res.status).toHaveBeenCalledWith(404)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Dish not found' })
+    })
+
+    it('should handle errors and return status 500', async () => {
+      req.params.customerId = 'testCustomerId'
+      req.body = {
+        restaurantId: 'testRestaurantId',
+        dishId: 'testDishId',
+        quantity: 2,
+        note: 'Extra sauce'
+      }
+      Item.findOne.mockRejectedValue(new Error('Database error'))
+      await customerController.addItemToCart(req, res)
+      expect(Item.findOne).toHaveBeenCalledWith({ 'dish._id': 'testDishId' })
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Database error' })
+    })
+  })
+
+  describe('acceptOrder', () => {
+    let req, res
+    beforeEach(() => {
+      req = { params: { orderId: 'testOrderId' } }
+      res = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      }
+    })
+
+    it('should accept the order when status is "Pending"', async () => {
+      const updatedOrder = { _id: 'testOrderId', status: 'Accepted' }
+      Order.findOneAndUpdate.mockResolvedValue(updatedOrder)
+      await customerController.acceptOrder(req, res)
+      expect(updatedOrder.status).toBe('Accepted')
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Order accepted successfully', order: updatedOrder })
+    })
+
+    it('should return 404 error when order ID is invalid or order is already accepted', async () => {
+      Order.findOneAndUpdate.mockResolvedValue(null)
+      await customerController.acceptOrder(req, res)
+      expect(res.status).toHaveBeenCalledWith(404)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Order not found or already accepted' })
+    })
+
+    it('should return 500 error when an error occurs during acceptance process', async () => {
+      const errorMessage = 'Internal server error'
+      Order.findOneAndUpdate.mockRejectedValue(new Error(errorMessage))
+      await customerController.acceptOrder(req, res)
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.json).toHaveBeenCalledWith({ message: errorMessage })
+    })
+  })
+
+  describe('cancelOrder', () => {
+    let req, res
+    beforeEach(() => {
+      req = { params: { orderId: 'testOrderId' } }
+      res = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      }
+    })
+
+    it('should cancel the order when status is "Accepted"', async () => {
+      const order = { _id: 'testOrderId', status: 'Accepted', save: jest.fn() }
+      Order.findById.mockResolvedValue(order)
+      await customerController.cancelOrder(req, res)
+      expect(order.status).toBe('Canceled')
+      expect(order.save).toHaveBeenCalled()
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Order canceled successfully', order })
+    })
+
+    it('should cancel the order when status is "Preparing"', async () => {
+      const order = { _id: 'testOrderId', status: 'Preparing', save: jest.fn() }
+      Order.findById.mockResolvedValue(order)
+      await customerController.cancelOrder(req, res)
+      expect(order.status).toBe('Canceled')
+      expect(order.save).toHaveBeenCalled()
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Order canceled successfully', order })
+    })
+
+    it('should return 404 error when order ID is invalid', async () => {
+      Order.findById.mockResolvedValue(null)
+      await customerController.cancelOrder(req, res)
+      expect(res.status).toHaveBeenCalledWith(404)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Order not found' })
+    })
+
+    it('should return 500 error when an error occurs during cancellation process', async () => {
+      const errorMessage = 'Internal server error'
+      Order.findById.mockRejectedValue(new Error(errorMessage))
+      await customerController.cancelOrder(req, res)
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.json).toHaveBeenCalledWith({ message: errorMessage })
+    })
+  })
+
+  describe('removeItemFromCart', () => {
+    let req, res
+    beforeEach(() => {
+      req = { body: {} }
+      res = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      }
+    })
+
+    it('should remove an item from the cart and return the updated order', async () => {
+      req.body.customerId = 'testCustomerId'
+      req.body.restaurantId = 'testRestaurantId'
+      req.body.itemId = 'testItemId'
+      const mockOrder = {
+        _id: 'testOrderId',
+        customer: 'testCustomerId',
+        restaurant: 'testRestaurantId',
+        status: 'Pending',
+        items: [
+          { _id: 'testItemId', price: 10, quantity: 2 }
+        ],
+        total_price: 20,
+        save: jest.fn()
+      }
+      Order.findOne.mockResolvedValue(mockOrder)
+      await customerController.removeItemFromCart(req, res)
+      expect(Order.findOne).toHaveBeenCalledWith({ customer: 'testCustomerId', restaurant: 'testRestaurantId', status: 'Pending' })
+      expect(mockOrder.items.length).toBe(0)
+      expect(mockOrder.total_price).toBe(0)
+      expect(mockOrder.save).toHaveBeenCalled()
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Item removed from cart successfully', order: mockOrder })
+    })
+
+    it('should handle item not found in cart and return status 404', async () => {
+      req.body.customerId = 'testCustomerId'
+      req.body.restaurantId = 'testRestaurantId'
+      req.body.itemId = 'nonExistingItemId'
+      Order.findOne.mockResolvedValue(null)
+      await customerController.removeItemFromCart(req, res)
+      expect(Order.findOne).toHaveBeenCalledWith({ customer: 'testCustomerId', restaurant: 'testRestaurantId', status: 'Pending' })
+      expect(res.status).toHaveBeenCalledWith(404)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Order not found or already processed' })
+    })
+
+    it('should handle errors and return status 500', async () => {
+      req.body.customerId = 'testCustomerId'
+      req.body.restaurantId = 'testRestaurantId'
+      req.body.itemId = 'testItemId'
+      Order.findOne.mockRejectedValue(new Error('Database error'))
+      await customerController.removeItemFromCart(req, res)
+      expect(Order.findOne).toHaveBeenCalledWith({ customer: 'testCustomerId', restaurant: 'testRestaurantId', status: 'Pending' })
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Database error' })
+    })
+  })
+})
---
+++ server/server/controllers/customer.js
@@ -0,0 +1,203 @@
+const RestaurantModel = require('../models/restaurant')
+const Order = require('../models/order')
+const { Item } = require('../models/item_dish')
+const { Dish } = require('../models/item_dish')
+
+const customerController = {}
+
+customerController.proveOfLife = async (req, res) => {
+  try {
+    res.status(200).json({ message: 'Alive' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
+
+// Get All Restaurants:
+
+customerController.getAllRestaurants = async (req, res) => {
+  try {
+    const restaurants = await RestaurantModel.find()
+    res.status(200).json(restaurants)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Get Restaurant By ID:
+
+customerController.getRestaurantById = async (req, res) => {
+  const { id } = req.params
+  try {
+    const restaurant = await RestaurantModel.findById(id)
+
+    if (!restaurant) {
+      return res.status(404).json({ message: 'Restaurant not found' })
+    }
+    res.status(200).json(restaurant)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Get All Dishes:
+
+customerController.getAllDishes = async (req, res) => {
+  try {
+    const dishes = await Dish.find()
+    res.status(200).json({ dishes })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Get A certain Dish By Id
+
+customerController.getDishById = async (req, res) => {
+  const { dishId } = req.params
+  try {
+    const dish = await Dish.findById(dishId)
+    if (!dish) {
+      return res.status(404).json({ message: 'Dish not found' })
+    }
+    const restaurant = await RestaurantModel.findById(dish.restaurant)
+    if (!restaurant) {
+      return res.status(404).json({ message: 'Restaurant not found for the dish' })
+    }
+    const restaurantName = restaurant.name
+    res.status(200).json({ dish, restaurantName })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Get All dishes Of A Certain Restaurant
+
+customerController.getAllDishesOfRestaurant = async (req, res) => {
+  const { restaurantId } = req.params
+  try {
+    // Find all dishes for the specified restaurant
+    const dishes = await Dish.find({ restaurant: restaurantId })
+    res.status(200).json({ dishes })
+  } catch (error) {
+    console.error('Error fetching dishes:', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
+// Get All Orders By UserId
+
+customerController.getAllOrdersByUserId = async (req, res) => {
+  const userId = req.params.userId
+  try {
+    const orders = await Order.find({ customer: userId })
+    res.status(200).json({ orders })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Add An Item To Cart
+
+customerController.addItemToCart = async (req, res) => {
+  const { customerId } = req.params
+  const { restaurantId, dishId, quantity, note } = req.body
+  try {
+    const item = await Item.findOne(({ 'dish._id': dishId }))
+    if (!item) {
+      return res.status(404).json({ message: 'Dish not found' })
+    }
+    const totalPrice = item.dish.price * quantity
+    const order = await Order.findOneAndUpdate(
+      { customer: customerId, restaurant: restaurantId, status: 'Pending' },
+      { $push: { items: { 'dish._id': dishId, quantity, price: totalPrice, note } } },
+      { new: true, upsert: true }
+    )
+    order.price += totalPrice
+    await order.save()
+    res.status(201).json({ message: 'Item added to cart successfully', order })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Accept Order
+
+customerController.acceptOrder = async (req, res) => {
+  const orderId = req.params.orderId
+  try {
+    // Find the order by its ID and update its status to 'Accepted'
+    const updatedOrder = await Order.findOneAndUpdate(
+      { _id: orderId, status: 'Pending' },
+      { status: 'Accepted' },
+      { new: true }
+    )
+    if (!updatedOrder) {
+      return res.status(404).json({ message: 'Order not found or already accepted' })
+    }
+    res.status(200).json({ message: 'Order accepted successfully', order: updatedOrder })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Cancel Order
+
+customerController.cancelOrder = async (req, res) => {
+  const orderId = req.params.orderId
+  try {
+    // Find the order by its ID
+    const order = await Order.findById(orderId)
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' })
+    }
+    if (order.status !== 'Accepted' && order.status !== 'Preparing') {
+      return res.status(400).json({ message: 'Order cannot be canceled because its status is not Accepted or Preparing' })
+    }
+    order.status = 'Canceled'
+    await order.save()
+    res.status(200).json({ message: 'Order canceled successfully', order })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Remove Item From Cart
+
+customerController.removeItemFromCart = async (req, res) => {
+  const { customerId, restaurantId, itemId } = req.body
+
+  try {
+    const order = await Order.findOne({ customer: customerId, restaurant: restaurantId, status: 'Pending' })
+
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found or already processed' })
+    }
+    const itemIndex = order.items.findIndex(item => item._id.toString() === itemId)
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: 'Item not found in cart' })
+    }
+    const removedItem = order.items.splice(itemIndex, 1)[0]
+    order.total_price -= removedItem.price * removedItem.quantity
+    await order.save()
+    res.status(200).json({ message: 'Item removed from cart successfully', order })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Get Pending Orders (Cart)
+
+customerController.getPendingOrders = async (req, res) => {
+  const { customerId, restaurantId } = req.params
+  try {
+    const pendingOrders = await Order.find({ customer: customerId, restaurant: restaurantId, status: 'Pending' })
+      .populate('items')
+    res.status(200).json({ orders: pendingOrders })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+module.exports = customerController

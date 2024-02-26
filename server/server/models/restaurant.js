bef3962c851e292cb736b258372d25a9cde19cc5
danaomar <danaomaark@gmail.com>
2024-02-26 14:05:35 +0300

    build restaurant modle, controller, route

--- server/server/models/restaurant.js
+++ server/server/models/restaurant.js
@@ -1,13 +1,56 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const restaurantSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
     title: {
         type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        validate: {
+            validator: value => validator.isEmail(value),
+            message: 'Invalid email format'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        validate: {
+          validator: function(password) {
+            const minLength = 8;
+            const hasUppercase = /[A-Z]/;
+            const hasLowercase = /[a-z]/;
+            const hasNumber = /[0-9]/;
+            const hasSpecialChar = /[\W_]/; // Matches any non-word character or underscore
+    
+            return validator.isLength(password, { min: minLength })
+              && hasUppercase.test(password)
+              && hasLowercase.test(password)
+              && hasNumber.test(password)
+              && hasSpecialChar.test(password);
+          },
+          message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+        }
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone number is required'],
+        unique: true,
+        validate: {
+            validator: function(phone) {
+                const regex = /^(78|77|79)\d{7}$/;
+                return regex.test(phoneNumber);
+            },
+            message: 'Invalid phone number.'
+        }
+    },
+    role: {
+        type: String,
+        enum: ['Customer', 'Rider', 'Restaurant', 'Admin'],
+        default: 'Customer',
         required: true
     },
     license: {
@@ -34,7 +77,7 @@ const restaurantSchema = new mongoose.Schema({
         max: 5,
     },
     deliveryTime: {
-        type: Date,
+        type: Number,
         required: true,
     },
     deliveryFee: {
@@ -45,7 +88,11 @@ const restaurantSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Order',
         required: true
-    }]
+    }],
+    registered_at: {
+        type: Date,
+        default: Date.now,
+    },
 }, { timestamps: true }); 
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
--- server/server/routes/customer.js
+++ server/server/routes/customer.js
@@ -1,18 +1,20 @@
 const express  = require("express");
 const router = express.Router();
 const customerController = require("../controllers/customer");
+const { authenticate } = require('../middleware/auth');
+const { checkCustomerRole } = require('../middleware/verify');
 
-router.get("/",customerController.proveOfLife);
-router.get('/restaurants', customerController.getAllRestaurants);
-router.get('/restaurant/:id', customerController.getRestaurantById);
-router.get('/dishes', customerController.getAllDishes);
-router.get('/dishes/:dishId', customerController.getDishById);
-router.get('/dishes/:restaurantId', customerController.getAllDishesOfRestaurant);
-router.get('/orders/:userId', customerController.getAllOrdersByUserId);
-router.get('/orders/pending/:customerId/:restaurantId', customerController.getPendingOrders);
-router.post('/cart/customerId', customerController.addItemToCart);
-router.put('/orders/:orderId/accept', customerController.acceptOrder);
-router.put('/orders/:orderId/cancel', customerController.cancelOrder);
-router.delete('/cart', customerController.removeItemFromCart);
+router.get("/", authenticate, checkCustomerRole, customerController.proveOfLife);
+router.get('/restaurants', authenticate, checkCustomerRole, customerController.getAllRestaurants);
+router.get('/restaurant/:id', authenticate, checkCustomerRole, customerController.getRestaurantById);
+router.get('/dishes', authenticate, checkCustomerRole, customerController.getAllDishes);
+router.get('/dishes/:dishId', authenticate, checkCustomerRole, customerController.getDishById);
+router.get('/dishes/:restaurantId', authenticate, checkCustomerRole, customerController.getAllDishesOfRestaurant);
+router.get('/orders/:userId', authenticate, checkCustomerRole, customerController.getAllOrdersByUserId);
+router.get('/orders/pending/:customerId/:restaurantId', authenticate, checkCustomerRole, customerController.getPendingOrders);
+router.post('/cart/customerId', authenticate, checkCustomerRole, customerController.addItemToCart);
+router.put('/orders/:orderId/accept', authenticate, checkCustomerRole, customerController.acceptOrder);
+router.put('/orders/:orderId/cancel', authenticate, checkCustomerRole, customerController.cancelOrder);
+router.delete('/cart', authenticate, checkCustomerRole, customerController.removeItemFromCart);
 
 module.exports = router;

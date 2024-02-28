bf19b73aa244f9fda80c508614ebf02d35d82848
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-02-28 17:27:45 +0300

    unwanted change

--- server/server/models/restaurant.js
+++ server/server/models/restaurant.js
@@ -1,19 +1,76 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const restaurantSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
     title: {
         type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        validate: {
+            validator: value => validator.isEmail(value),
+            message: 'Invalid email format'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        validate: {
+          validator: function(password) {
+            const minLength = 8;
+            const hasUppercase = /[A-Z]/;
+            const hasLowercase = /[a-z]/;
+            const hasNumber = /[0-9]/;
+            const hasSpecialChar = /[\W_]/; // Matches any non-word character or underscore
+    
+            return validator.isLength(password, { min: minLength })
+              && hasUppercase.test(password)
+              && hasLowercase.test(password)
+              && hasNumber.test(password)
+              && hasSpecialChar.test(password);
+          },
+          message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+        }
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone number is required'],
+        unique: true,
+        validate: {
+            validator: function(phone) {
+                const regex = /^(78|77|79)\d{7}$/;
+                return regex.test(phone);
+            },
+            message: 'Invalid phone number.'
+        }
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required'],
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['Customer', 'Rider', 'Restaurant', 'Admin'],
+        default: 'Restaurant',
         required: true
     },
     license: {
         type: String,
         required: true,
         unique: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    buildingNo: {
+        type: Number,
+        required: true
     },
     area: {
         type: String,
@@ -23,18 +80,13 @@ const restaurantSchema = new mongoose.Schema({
         type: [String],
         required: true
     },
-    menu: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Dishes',
-        required: true
-    }],
     rate: {
         type: Number,
         min: 0,
         max: 5,
     },
     deliveryTime: {
-        type: Date,
+        type: Number,
         required: true,
     },
     deliveryFee: {
@@ -45,7 +97,12 @@ const restaurantSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Order',
         required: true
-    }]
+    }],
 }, { timestamps: true }); 
+  
+// Compare the given password with the hashed password in the database
+restaurantSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);

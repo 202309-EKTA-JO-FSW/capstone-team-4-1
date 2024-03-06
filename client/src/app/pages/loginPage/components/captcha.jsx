093de65a2097d98ff7ad96fc46f997b9508c1431
unknown <mahmoudrmn@gmail.com>
2024-03-06 15:19:10 +0300

    build login page

--- client/src/app/pages/loginPage/components/captcha.jsx
+++ client/src/app/pages/loginPage/components/captcha.jsx
@@ -8,7 +8,11 @@ const Captcha = ({ onChange }) => {
   }, [onChange]);
 
   return (
-    <div className="w-full flex justify-center transform scale-75">
+    <div className="flex justify-center transform
+    xl:scale-75 xl:-my-2
+    md:scale-65 md:-my-3
+    2xs:scale-50 2xs:-my-3
+    ">
       <ReCAPTCHA
         sitekey="6LfG1YspAAAAAOqk5dBqT4T7x42tIxBwLKnu56xn"
         onChange={onCaptchaChange}

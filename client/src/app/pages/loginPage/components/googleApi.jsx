01e3aad6392dc7cdf3641e117b1649b69a784fe3
unknown <mahmoudrmn@gmail.com>
2024-03-06 15:23:24 +0300

    build login page

--- client/src/app/pages/loginPage/components/googleApi.jsx
+++ client/src/app/pages/loginPage/components/googleApi.jsx
@@ -7,7 +7,8 @@ const clientId = "63738745837-k81ls7845ijo98r0k1bk2ktoema1akf0.apps.googleuserco
 function App() {
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <div className="justify-center transform scale-75">
+      <div className="justify-center transform 
+      xl:scale-75 md:scale-55 2xs:scale-40">
         <LoginPage />
       </div>
     </GoogleOAuthProvider>

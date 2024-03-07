6642f1409c50f3e385dd33720310392b1a5b1122
MahmoudRumaneh <mahmoudrmn@gmail.com>
2024-03-07 21:13:55 +0300

    Update index.js

--- client/src/app/pages/customerSignup/index.js
+++ client/src/app/pages/customerSignup/index.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/navbar";
-import Footer from "../../components/footer/Footer";
+import Footer from "../../components/footer/footer";
 import Location from "./components/location";
 
 export default function CustomerSignup() {

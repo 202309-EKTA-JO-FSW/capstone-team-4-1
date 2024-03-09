fcc71056033bac46ce2428a6cd82b34d56c847d3
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-03-09 04:46:50 +0300

    Feat: add signout

--- server/server/routes/customer.js
+++ server/server/routes/customer.js
@@ -6,16 +6,16 @@ const { verifyUser } = require('../middleware/verify');
 
 const customerController = require('../controllers/customer');
 
-router.get('/restaurants', customerController.getAllRestaurants);
-router.get('/restaurant/:id', customerController.getRestaurantById);
-router.get('/dishes', customerController.getAllDishes);
-router.get('/dishes/:dishId', customerController.getDishById);
-router.get('/dishes/restaurant/:restaurantId', customerController.getAllDishesOfRestaurant);
-// router.get('/restaurants', authenticate, verifyUser(['Customer','Admin']), customerController.getAllRestaurants);
-// router.get('/restaurant/:id', authenticate, verifyUser(['Customer','Admin']), customerController.getRestaurantById);
-// router.get('/dishes', authenticate, verifyUser(['Customer','Admin']), customerController.getAllDishes);
-// router.get('/dishes/:dishId', authenticate, verifyUser(['Customer','Admin']), customerController.getDishById);
-// router.get('/dishes/restaurant/:restaurantId', authenticate, verifyUser(['Customer','Admin']), customerController.getAllDishesOfRestaurant);
+// router.get('/restaurants', customerController.getAllRestaurants);
+// router.get('/restaurant/:id', customerController.getRestaurantById);
+// router.get('/dishes', customerController.getAllDishes);
+// router.get('/dishes/:dishId', customerController.getDishById);
+// router.get('/dishes/restaurant/:restaurantId', customerController.getAllDishesOfRestaurant);
+router.get('/restaurants', authenticate, verifyUser(['customer','admin']), customerController.getAllRestaurants);
+router.get('/restaurant/:id', authenticate, verifyUser(['customer','admin']), customerController.getRestaurantById);
+router.get('/dishes', authenticate, verifyUser(['customer','admin']), customerController.getAllDishes);
+router.get('/dishes/:dishId', authenticate, verifyUser(['customer','admin']), customerController.getDishById);
+router.get('/dishes/restaurant/:restaurantId', authenticate, verifyUser(['customer','admin']), customerController.getAllDishesOfRestaurant);
 router.get('/orders/:customerId', authenticate, verifyCustomer, customerController.getAllOrdersByCustomerId);
 router.get('/orders/pending/:customerId/:restaurantId', authenticate, verifyCustomer, customerController.getPendingOrders);
 router.post('/cart', authenticate, verifyCustomer, customerController.addItem);

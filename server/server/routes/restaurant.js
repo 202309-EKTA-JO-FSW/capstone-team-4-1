488d3fb8e07b3fb7a685f96390ddb3574d16a73d
danaomar <danaomaark@gmail.com>
2024-03-09 20:10:47 +0300

    bug fix

--- server/server/routes/restaurant.js
+++ server/server/routes/restaurant.js
@@ -8,7 +8,7 @@ const restaurantController = require('../controllers/restaurant');
 router.get("/profile/:restaurantId", restaurantController.getProfile);
 router.get("/menu/:restaurantId", restaurantController.getAllDishes);
 router.get("/dish/:dishId", authenticate, verifyRestauarant, restaurantController.getSingleDish);
-router.get("/dish/:dishId", authenticate, verifyRestauarant, restaurantController.searchDish);
+router.get("/:restaurantId/search", authenticate, verifyRestauarant, restaurantController.searchDish);
 router.get("/orders/:restaurantId", authenticate, verifyRestauarant, restaurantController.getOrders);
 router.get("/order/:orderId", authenticate, verifyRestauarant, restaurantController.getOrder);
 router.post("/dish/:restaurantId", restaurantController.addDish);

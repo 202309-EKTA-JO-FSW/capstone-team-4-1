02085ea1cbdbb2e8717cfbb216035b9044c26101
danaomar <danaomaark@gmail.com>
2024-03-11 15:48:43 +0300

    file clean up

--- client/src/app/pages/customer/addItem/[dishID]/page.jsx
+++ client/src/app/pages/customer/addItem/[dishID]/page.jsx
@@ -1,11 +1,8 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import LoadingAnimation from '../../../../components/loadingAnimation';
-import helpers from "../../../../services/helpers";
 
 export default function AddItem({ dishId }) {
-  console.log(dishId);
   const dishID  = dishId;
   const [item, setItem] = useState();
 
@@ -15,27 +12,22 @@ export default function AddItem({ dishId }) {
       Authorization: `Bearer ${token}`
     };
 
-    fetch (`http://localhost:3001/customer/dishes/${dishID}`,{
+    fetch(`http://localhost:3001/customer/dishes/${dishID}`,{
         headers: headers
     })
-        .then(res => res.json())
-        .then(data => setItem(data))
+      .then(res => res.json())
+      .then(data => setItem(data))
   }, [dishID]);
 
-    
-console.log(item)
+
   if (!item) { 
     return <LoadingAnimation />; 
   }
 
   
   return (
-    
     <div className="z-50 fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-      <div className="relative mx-auto my-0 top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 border w-[650px] shadow-lg rounded-3xl bg-white
-       xl:p-5 xl:w-[650px] xl:h-auto xl:rounded-3xl xl:shadow-lg
-       md:p-3 md:w-[400px] md:h-auto md:rounded-2xl md:shadow-sm
-       2xs:p-1 2xs:w-[250px] 2xs:h-auto 2xs:rounded-xl 2xs:shadow-xs">
+      <div className="relative mx-auto my-0 top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 border w-[650px] shadow-lg rounded-3xl bg-white xl:p-5 xl:w-[650px] xl:h-auto xl:rounded-3xl xl:shadow-lg md:p-3 md:w-[400px] md:h-auto md:rounded-2xl md:shadow-sm 2xs:p-1 2xs:w-[250px] 2xs:h-auto 2xs:rounded-xl 2xs:shadow-xs">
         <div className="space-y-4 pt-10">
           <div className="text-center p-2 bg-green-100 text-green-700 rounded-md">Dish Information</div>
           <div>

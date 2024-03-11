e99b1fd2a1b83f1b846b60a34a19e1b1bca16fcf
danaomar <danaomaark@gmail.com>
2024-03-11 22:43:47 +0300

    feat customer profile page

--- client/src/app/pages/customer/[customerId]/page.jsx
+++ client/src/app/pages/customer/[customerId]/page.jsx
@@ -32,8 +32,21 @@ const CusotmerProfile = () => {
     return ( 
         <div> 
             {/* <Navbar /> */}
-            <div>
-                <h1>Hi Customer, {customer.firstName}</h1>
+            <div className='max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900'>
+                <div className='rounded-t-lg h-32 overflow-hidden'>
+                    <img src="/blur-restaurant.jpg" alt="background" className='object-cover object-top w-full' />
+                </div>
+                <div className='mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden'>
+                    <img src="https://cdn0.iconfinder.com/data/icons/communication-456/24/account_profile_user_contact_person_avatar_placeholder-512.png" alt="profile picture" className='object-cover object-center h-32' />
+                </div>
+                <div className='text-center mt-2'>
+                    <h2 className="font-semibold">{customer.firstName} {customer.lastName}</h2>
+                    <p className="text-gray-500">{customer.email}</p>
+                    <p className="text-gray-500">0{customer.phone}</p>
+                    <p className="text-gray-500">Balance: {customer.balance}</p>
+                    <p className="text-gray-500">Street: {customer.street}</p>
+                    <p className="text-gray-500">Building Number: {customer.buildingNo}</p>
+                </div>
             </div>
             {/* <Footer /> */}
         </div> 

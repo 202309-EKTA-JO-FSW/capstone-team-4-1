d356e7bf002bd06854f252b73f573676ce213d25
unknown <mahmoudrmn@gmail.com>
2024-03-11 17:54:13 +0300

    about us page

--- client/src/app/pages/AboutPage/page.js
+++ client/src/app/pages/AboutPage/page.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Footer from '@/app/components/footer/footer';
-
-
 const AboutPage = () => {
   return (
       <div className="relative bg-[#fff] overflow-hidden text-black">
@@ -16,15 +14,25 @@ const AboutPage = () => {
           alt="gifwelcome"
         />
 
-          <div className="flex absolute bottom-0 mb-4 w-full justify-center z-10">
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><InstagramIcon className="text-white w-6 h-6 mx-2" /></a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FacebookIcon className="text-white w-6 h-6 mx-2" /></a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><TwitterIcon className="text-white w-6 h-6 mx-2" /></a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><LinkedInIcon className="text-white w-6 h-6 mx-2" /></a>
-            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer"><YouTubeIcon className="text-white w-6 h-6 mx-2" /></a>
+          <div className="flex absolute top-0 bottom-0 z-10 justify-between px-4 xl:mt-4 xl:ml-[4rem] space-x-4 opacity-70">
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <svg fill="#ffff" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1">
+                <path d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"/>
+              </svg>
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <svg fill="#ffff" width="20px" height="20px" viewBox="-7 -2 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-facebook"><path d='M2.046 3.865v2.748H.032v3.36h2.014v9.986H6.18V9.974h2.775s.26-1.611.386-3.373H6.197V4.303c0-.343.45-.805.896-.805h2.254V0H6.283c-4.34 0-4.237 3.363-4.237 3.865z' /></svg>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <svg fill="#ffff" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M19,4 C20.6568542,4 22,5.34314575 22,7 L22,17 C22,18.6568542 20.6568542,20 19,20 L5,20 C3.34314575,20 2,18.6568542 2,17 L2,7 C2,5.34314575 3.34314575,4 5,4 L19,4 Z M20,7.328 L12.6585046,13.7525767 C12.3128975,14.054983 11.8110564,14.0801835 11.4394103,13.8281783 L11.3414954,13.7525767 L4,7.329 L4,17 C4,17.5522847 4.44771525,18 5,18 L19,18 C19.5522847,18 20,17.5522847 20,17 L20,7.328 Z M18.48,6 L5.518,6 L12,11.6712318 L18.48,6 Z"/>
+            </svg>
+            </a>
+           
           </div>
+          <div className="border-t border-gray-100 mt-[3.5rem] mx-[4rem] obacity-70"></div>
 
-        <div className="xl:mt-[2rem] xl:ml-[4rem]">
+        <div className="xl:mt-8 xl:ml-10">
           <img className="absolute xl:w-[200px] xl:h-auto" 
           src="/logo.png"
           alt=""
@@ -32,13 +40,12 @@ const AboutPage = () => {
         </div>
 
         <div
-          className="absolute w-full  flex items-center justify-center z-10 
-            xl:h-[600px] lg:h-[400px] md:h-[400px] sm:h-[300px] h-[200px]"
+          className="absolute w-full  flex items-center justify-center z-10"
         >
           <h1
             className="font-bold text-white text-center
               xl:text-6xl lg:text-4xl md:text-xl sm:text-lg text-md
-               xl:pt-10 lg:pt-4 sm:pt-2 pt-4 xl:mt-10 lg:mt-10 sm:mt-6 mt-4
+               xl:pt-[7rem] lg:pt-4 sm:pt-2 pt-4 xl:mt-10 lg:mt-10 sm:mt-6 mt-4
                xl:px-[10rem]
                "
           >
@@ -47,17 +54,13 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <svg
-        className="relative xl:-mt-12 lg:-mt-8 md:-mt-6 sm:-mt-4 2xs:-mt-4"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 80"
-      >
-        <path
-          fill="#ffffff"
-          fillOpacity="1"
-          d="M0,40L60,43.3C120,47,240,53,360,53.3C480,53,600,43,720,36.7C840,31,960,29,1080,36.7C1200,43,1320,57,1380,62.7L1440,64L1440,80L1380,80C1320,80,1200,80,1080,80C960,80,840,80,720,80C600,80,480,80,360,80C240,80,120,80,60,80L0,80Z"
-        ></path>
+      <svg 
+      className="relative xl:-mt-[4rem] lg:-mt-8 md:-mt-6 sm:-mt-4 2xs:-mt-4"
+      xmlns="http://www.w3.org/2000/svg"
+       viewBox="0 0 1440 80">
+        <path fill="#ffffff" fillOpacity="1" d="M0,64L120,54.7C240,45,480,27,720,25.3C960,24,1200,40,1320,48L1440,56L1440,80L1320,80C1200,80,960,80,720,80C480,80,240,80,120,80L0,80Z"></path>
       </svg>
+
       <Footer />
       </div>
   )

5633045a6f42f50509fe1dcaa33d19e39b20cb6d
danaomar <danaomaark@gmail.com>
2024-03-11 22:10:47 +0300

    fetch customer

--- client/src/app/pages/customer/[customerId]/page.jsx
+++ client/src/app/pages/customer/[customerId]/page.jsx
@@ -1,12 +1,41 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from "next/navigation";
 import Navbar from "@/app/components/navbar/navbar";
 import Footer from "@/app/components/footer/footer";
 
 const CusotmerProfile = () => { 
+    const params = useParams();
+    const customerId = params.customerId;
+    const [customer, setCustomer] = useState();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const headers = {
+            Authorization: `Bearer ${token}`
+        };
+        fetch(`http://localhost:3001/customer/profile/${customerId}`, {
+      headers: headers
+    })
+        .then(res => res.json())
+        .then(data => setCustomer(data))
+    }, [customerId]);
+
+    if (!customer) {
+        return (
+            <div>Loading...</div>
+        )
+    } else {
+        console.log(customer);
+    }
+
     return ( 
         <div> 
-            <h1>Hi there Customer</h1> 
+            {/* <Navbar /> */}
+            <div>
+                <h1>Hi Customer, {customer.firstName}</h1>
+            </div>
+            {/* <Footer /> */}
         </div> 
     ); 
 }

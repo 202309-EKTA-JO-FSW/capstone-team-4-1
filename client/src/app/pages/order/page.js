3eab905894a71432b2bd8d57a7e61a49cbc745dc
danaomar <danaomaark@gmail.com>
2024-03-17 18:02:51 +0300

    fix bug

--- client/src/app/pages/order/page.js
+++ client/src/app/pages/order/page.js
@@ -64,7 +64,7 @@ const Order = () => {
     };
 
     if(cartItems && cartItems.length > 0) {
-      
+      console.log("Data:", orderData);
       try {
         await Promise.all(cartItems.map(async (item) => {
             const response = await fetch(`http://localhost:3001/customer/cart`, {
@@ -368,7 +368,7 @@ const Order = () => {
               <div className="mt-2">
                 <label className="flex items-center justify-between bg-white px-4 py-2 border border-[#FFC254] rounded-3xl leading-tight focus:outline-none cursor-pointer">
                   <div className="flex items-center">
-                   <svg className="pt-1/2 pr-2" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 13C7 11.1144 7 10.1716 7.58579 9.58579C8.17157 9 9.11438 9 11 9H14H17C18.8856 9 19.8284 9 20.4142 9.58579C21 10.1716 21 11.1144 21 13V14V15C21 16.8856 21 17.8284 20.4142 18.4142C19.8284 19 18.8856 19 17 19H14H11C9.11438 19 8.17157 19 7.58579 18.4142C7 17.8284 7 16.8856 7 15V14V13Z" stroke="#FFC254" stroke-width="2" stroke-linejoin="round"></path> <path d="M7 15V15C5.11438 15 4.17157 15 3.58579 14.4142C3.58579 14.4142 3.58579 14.4142 3.58579 14.4142C3 13.8284 3 12.8856 3 11L3 9C3 7.11438 3 6.17157 3.58579 5.58579C4.17157 5 5.11438 5 7 5L13 5C14.8856 5 15.8284 5 16.4142 5.58579C17 6.17157 17 7.11438 17 9V9" stroke="#FFC254" stroke-width="2" stroke-linejoin="round"></path> <path d="M16 14C16 15.1046 15.1046 16 14 16C12.8954 16 12 15.1046 12 14C12 12.8954 12.8954 12 14 12C15.1046 12 16 12.8954 16 14Z" stroke="#FFC254" stroke-width="2"></path> </g></svg>
+                   <svg className="pt-1/2 pr-2" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 13C7 11.1144 7 10.1716 7.58579 9.58579C8.17157 9 9.11438 9 11 9H14H17C18.8856 9 19.8284 9 20.4142 9.58579C21 10.1716 21 11.1144 21 13V14V15C21 16.8856 21 17.8284 20.4142 18.4142C19.8284 19 18.8856 19 17 19H14H11C9.11438 19 8.17157 19 7.58579 18.4142C7 17.8284 7 16.8856 7 15V14V13Z" stroke="#FFC254" strokeWidth="2" strokeLinejoin="round"></path> <path d="M7 15V15C5.11438 15 4.17157 15 3.58579 14.4142C3.58579 14.4142 3.58579 14.4142 3.58579 14.4142C3 13.8284 3 12.8856 3 11L3 9C3 7.11438 3 6.17157 3.58579 5.58579C4.17157 5 5.11438 5 7 5L13 5C14.8856 5 15.8284 5 16.4142 5.58579C17 6.17157 17 7.11438 17 9V9" stroke="#FFC254" strokeWidth="2" strokeLinejoin="round"></path> <path d="M16 14C16 15.1046 15.1046 16 14 16C12.8954 16 12 15.1046 12 14C12 12.8954 12.8954 12 14 12C15.1046 12 16 12.8954 16 14Z" stroke="#FFC254" strokeWidth="2"></path> </g></svg>
                     Pay with cash
                   </div>
                   <input 
@@ -382,7 +382,7 @@ const Order = () => {
                 </label>
                 <label className="flex items-center justify-between mt-3 bg-white px-4 py-2 border border-[#FFC254] rounded-3xl leading-tight focus:outline-none cursor-pointer">
                   <div className="flex items-center">
-                    <svg className="pt-1/2 pr-2 " width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3.5 10H20.5" stroke="#FFC254" stroke-width="2" stroke-linecap="round"></path> <path d="M6 14H8" stroke="#FFC254" stroke-width="2" stroke-linecap="round"></path> <path d="M11 14H13" stroke="#FFC254" stroke-width="2" stroke-linecap="round"></path> <path d="M3 9C3 7.11438 3 6.17157 3.58579 5.58579C4.17157 5 5.11438 5 7 5H12H17C18.8856 5 19.8284 5 20.4142 5.58579C21 6.17157 21 7.11438 21 9V12V15C21 16.8856 21 17.8284 20.4142 18.4142C19.8284 19 18.8856 19 17 19H12H7C5.11438 19 4.17157 19 3.58579 18.4142C3 17.8284 3 16.8856 3 15V12V9Z" stroke="#FFC254" stroke-width="2" stroke-linejoin="round"></path> </g></svg>
+                    <svg className="pt-1/2 pr-2 " width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3.5 10H20.5" stroke="#FFC254" strokeWidth="2" strokeLinecap="round"></path> <path d="M6 14H8" stroke="#FFC254" strokeWidth="2" strokeLinecap="round"></path> <path d="M11 14H13" stroke="#FFC254" strokeWidth="2" strokeLinecap="round"></path> <path d="M3 9C3 7.11438 3 6.17157 3.58579 5.58579C4.17157 5 5.11438 5 7 5H12H17C18.8856 5 19.8284 5 20.4142 5.58579C21 6.17157 21 7.11438 21 9V12V15C21 16.8856 21 17.8284 20.4142 18.4142C19.8284 19 18.8856 19 17 19H12H7C5.11438 19 4.17157 19 3.58579 18.4142C3 17.8284 3 16.8856 3 15V12V9Z" stroke="#FFC254" strokeWidth="2" strokeLinejoin="round"></path> </g></svg>
                     Add a new card
                   </div>
                   <input 

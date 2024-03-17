0e3d20fb4ebc2f64be510f3b0d9b62e2b16a516d
danaomar <danaomaark@gmail.com>
2024-03-17 16:33:45 +0300

    modify customer controller

--- server/server/controllers/customer.js
+++ server/server/controllers/customer.js
@@ -344,7 +344,7 @@ const createOrder = async (req, res) => {
     console.log("This is the new order", newOrder);
     res.status(201).json({ message: "Order created successfully", order: newOrder });
   } catch (error) {
-    res.status(402).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 };
 

a49a6034625207bbf5e4a000a70e7056644d677a
Hadeel-Ali-Obaid <hadeelobaid71@gmail.com>
2024-03-18 01:49:30 +0300

    Feat: display and edit profile picture in customer profile

--- server/server/routes/customer.js
+++ server/server/routes/customer.js
@@ -1,27 +1,32 @@
-const express  = require('express');
+const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../middleware/auth');
 const { verifyCustomer } = require('../middleware/verify');
 const { verifyUser } = require('../middleware/verify');
-
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        // cb(null, Date.now() + '-' + file.originalname);
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
 const customerController = require('../controllers/customer');
 
-// router.get('/restaurants', customerController.getAllRestaurants);
-// router.get('/restaurant/:id', customerController.getRestaurantById);
-// router.get('/dishes', customerController.getAllDishes);
-// router.get('/dishes/:dishId', customerController.getDishById);
-// router.get('/dishes/restaurant/:restaurantId', customerController.getAllDishesOfRestaurant);
-router.get('/profile/:customerId', authenticate, verifyUser(['customer','admin']), customerController.getProfile);
-router.get('/restaurants', authenticate, verifyUser(['customer','admin']), customerController.getAllRestaurants);
-router.get('/restaurant/:id', authenticate, verifyUser(['customer','admin']), customerController.getRestaurantById);
-router.get('/dishes', authenticate, verifyUser(['customer','admin']), customerController.getAllDishes);
-router.get('/dishes/:dishId', authenticate, verifyUser(['customer','admin']), customerController.getDishById);
-router.get('/dishes/restaurant/:restaurantId', authenticate, verifyUser(['customer','admin']), customerController.getAllDishesOfRestaurant);
+router.get('/profile/:customerId', authenticate, verifyUser(['customer', 'admin']), customerController.getProfile);
+router.get('/restaurants', authenticate, verifyUser(['customer', 'admin']), customerController.getAllRestaurants);
+router.get('/restaurant/:id', authenticate, verifyUser(['customer', 'admin']), customerController.getRestaurantById);
+router.get('/dishes', authenticate, verifyUser(['customer', 'admin']), customerController.getAllDishes);
+router.get('/dishes/:dishId', authenticate, verifyUser(['customer', 'admin']), customerController.getDishById);
+router.get('/dishes/restaurant/:restaurantId', authenticate, verifyUser(['customer', 'admin']), customerController.getAllDishesOfRestaurant);
 router.get('/orders/:customerId', authenticate, verifyCustomer, customerController.getAllOrdersByCustomerId);
 router.get('/orders/pending/:customerId/:restaurantId', authenticate, verifyCustomer, customerController.getPendingOrders);
 router.get('/cart', authenticate, verifyCustomer, customerController.getCart);
 router.post('/cart', authenticate, verifyCustomer, customerController.addItem);
-router.put('/profile/:customerId', authenticate, verifyCustomer, customerController.editProfile);
+router.put('/profile/:customerId', authenticate, verifyCustomer, upload.single('img'), customerController.editProfile);
 router.delete('/cart', authenticate, verifyCustomer, customerController.removeItemFromCart);
 
 module.exports = router;

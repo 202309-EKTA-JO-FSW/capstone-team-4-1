374737c1f850293c50b5b42dd0fa33ba1bfb5d1c
unknown <mahmoudrmn@gmail.com>
2024-03-21 04:21:14 +0300

    fix animation

--- client/src/app/pages/loginPage/loginSuccessfully/page.jsx
+++ client/src/app/pages/loginPage/loginSuccessfully/page.jsx
@@ -1,16 +1,17 @@
-import LoginSuccessAnimation from "@/app/components/loginSuccessAnimation"
+// import LoginSuccessAnimation from "@/app/components/loginSuccessAnimation"
 
 const LoginSucessfully = () => {
     return(
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50 overflow-y-auto h-full w-full">
         <div
-          className="relative flex justify-center items-center mx-auto border w-[650px] h-[520px] shadow-lg rounded-3xl bg-white
-      xl:top-20 xl:p-5 xl:w-[650px] xl:h-[520px] xl:rounded-3xl xl:shadow-lg
-      md:top-20 md:p-3 md:w-[400px] md:h-[420px] md:rounded-2xl md:shadow-sm
-      2xs:top-20 2xs:p-1 2xs:w-[250px] 2xs:h-[340px] 2xs:rounded-xl 2xs:shadow-xs
+          className="relative flex justify-center items-center mx-auto -mt-6 mb-0   
+           xl:w-[650px] xl:h-[530px] xl:-px-5
+           md:top-20 md:w-[400px] md:h-[420px]
+            2xs:top-20 2xs:w-[250px] 2xs:h-[340px]
       "
         >
-            <LoginSuccessAnimation />
+            <img className="rounded-3xl"
+             src="https://i.pinimg.com/originals/35/f3/23/35f323bc5b41dc4269001529e3ff1278.gif" alt="loginSuccess" />
         </div>
         </div>
         

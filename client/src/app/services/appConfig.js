01453777bd1956488ed184a2799245673317cb64
Hadeel-Ali-Obaid <hadeelobaid71@gmail.com>
2024-03-24 03:16:03 +0300

    dynamic hostname7

--- client/src/app/services/appConfig.js
+++ client/src/app/services/appConfig.js
@@ -3,6 +3,6 @@ if (typeof window !== 'undefined') {
     isProduction = window.location.hostname != 'localhost';
 }
 const urlService = {};
-urlService.clientUrl = true ? 'https://capstone-team-4-1-front.vercel.app' : 'http://localhost:3000';
-urlService.serverUrl = true ? 'https://capstone-team-4-1-backend.vercel.app' : 'http://localhost:3001';
+urlService.clientUrl = isProduction ? 'https://capstone-team-4-1-front.vercel.app' : 'http://localhost:3000';
+urlService.serverUrl = isProduction ? 'https://capstone-team-4-1-backend.vercel.app' : 'http://localhost:3001';
 export default urlService;
